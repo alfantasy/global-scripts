658c9fa1ef2fc68fd7d1c1a7956b391999e161d0
alfantasy <alfantasyfn@gmail.com>
2025-06-24 09:26:12 +0400

    коммит

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 XUSER=$(loginctl list-sessions | awk '$1 ~ /^[0-9]+$/ { print $1 }' | while read session; do
-    user=$(loginctl show-session $session -p User --value)
-    active=$(loginctl show-session $session -p Active --value)
+    uid=$(loginctl show-session "$session" -p User --value)
+    active=$(loginctl show-session "$session" -p Active --value)
     if [[ "$active" == "yes" ]]; then
-        echo "$user"
+        id -nu "$uid"
         break
     fi
 done)
