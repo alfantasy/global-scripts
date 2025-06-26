7d1655fe9c81171a89bb0c51bb585d933ca9f188
alfantasy <alfantasyfn@gmail.com>
2025-06-26 08:59:22 +0400

    Обновление скрипта: waydroid-installer.sh Почти доделан сам скрипт, осталось проверить его работу.

--- debian/waydroid-installer.sh
+++ debian/waydroid-installer.sh
@@ -1,5 +1,49 @@
 install_waydroid() {
     echo -e "\033[94mПроверка доступности пакетов...\033[0m"
+    APT_UPDATE=$(apt-get update >/dev/null 2>&1 && echo "1" || echo "0")
+    STEP_UPDATE_APT="0"
+    STEP_INSTALL_REQUIRED="0"
+    STEP_ADD_REP_WAYDROID="0"
+
+    if [ "$APT_UPDATE" == "1" ]; then
+        echo -e "\033[94mПакеты обновлены.\033[0m"
+        STEP_UPDATE_APT="1"
+    else
+        echo -e "\033[94mПакеты не обновлены.\033[0m"
+        echo -e "\033[91mНевозможно проверить доступность пакетов. Проверьте /etc/apt/sources.list и Ваше интернет-соединение.\033[0m"
+        exit 1
+    fi
+
+    if [ "$STEP_UPDATE_APT" == "1" ]; then
+        if $(apt install curl ca-certificates -y >/dev/null 2>&1); then
+            echo -e "\033[94mУстановлены необходимые пакеты для продолжения: \033[97mcurl ca-certificates.\033[0m"
+            STEP_INSTALL_REQUIRED="1"
+        else
+            echo -e "\033[91mПакеты не установлены. Проверьте /etc/apt/sources.list и Ваше интернет-соединение.\033[0m"
+            exit 1
+        fi
+    fi
+
+    if [ "$STEP_INSTALL_REQUIRED" == "1" ]; then
+        echo -e "\033[94mДобавляется официальный репозиторий Waydroid...\033[0m"
+        if $(curl -s https://repo.waydro.id); then
+            echo -e "\033[94mРепозиторий Waydroid добавлен.\033[0m"
+            STEP_ADD_REP_WAYDROID="1"
+        else
+            echo -e "\033[91mРепозиторий Waydroid не добавлен. Проверьте Ваше интернет-соединение.\033[0m"
+            exit 1
+        fi
+    fi
+
+    if [ "$STEP_ADD_REP_WAYDROID" == "1" ]; then
+        echo -e "\033[94mУстановка Waydroid...\033[0m"
+        if $(apt install waydroid -y); then
+            echo -e "\033[94mWaydroid установлен.\033[0m"
+        else
+            echo -e "\033[91mWaydroid не установлен. Проверьте /etc/apt/sources.list и Ваше интернет-соединение.\033[0m"
+            exit 1
+        fi
+    fi
 }
 
 if [ "$(id -u)" != "0" ]; then
